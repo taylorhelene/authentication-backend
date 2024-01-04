5a3cd54d3dd6696278cfd60cf28efbcd35971c80
unknown <taylorhelene09@gmail.com>
2024-01-04 09:43:38 +0300

    server

--- mpesa.js
+++ mpesa.js
@@ -1,5 +1,5 @@
 const Mpesa = require('mpesa-node')
-const mpesaApi = new Mpesa({ consumerKey: 'LgdXPNQiI3kJlzXdy1WmH0yKGA8MgSqf', consumerSecret: '6AFzzzGcsnXrcV1G' })
+const mpesaApi = new Mpesa({ })
 // another instance
 // const instance = new Mpesa({ consumerKey: 'test', consumerSecret: 'test', environment: 'production' })
 mpesaApi
